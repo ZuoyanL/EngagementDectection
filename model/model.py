4035fc56dd568c729c742eb37e0e31b1652075fc
ZuoyanL <lizuoyan@pku.edu.cn>
2020-05-13 06:31:51 +0000

    model包：对模型的操作，如实例化、保存等, Zuoyan

--- model/model.py
+++ model/model.py
@@ -1,2 +1,17 @@
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+import torchvision
 import cv2
+
+def load_emotion_model(model_path):
+  return torch.load(model_path)
+
+def load_eye_model(model_path, some_params):
+  eye_model = EyeModel(model_path)
+  return eye_model
+
+def save_model(model):
+  pass
+
