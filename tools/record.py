18c50d3a6d20b9320ebad67109fbaf84939a4c2d
lizuoyan <lizuoyan@pku.edu.cn>
2020-05-23 21:18:05 +0800

    保存实验数据

---
+++ tools/record.py
@@ -0,0 +1,13 @@
+"""
+Records the experiments results.
+Both .JSON files or .txt files are proper to store results, or others.
+Besured of using unified file naming rules.
+
+Be attention to the file naming rules.
+Such like, ResNet18_EYEAModel_params.json/.txt
+indicates a result of an experiment which used RerNet18 and eye model A with some parameters.
+"""
+
+def record(record_path = './results/', other_params=None):
+    # do something...
+    pass
